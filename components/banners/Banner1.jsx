51b716c0ea74936c78a3af4ad8261e3223d8b169
itarikahmed <tarik.ahmed@northsouth.edu>
2024-11-03 17:43:50 +0600

    assets bug fix

--- components/banners/Banner1.jsx
+++ components/banners/Banner1.jsx
@@ -1,8 +1,7 @@
 "use client";
-import React from "react";
+import Image from "next/image";
 import Banner from "@/assets/banner1.jpg";
 import { motion } from "framer-motion";
-import Image from "next/image";
 
 import { SlideUp } from "@/lib/animation/animate";
 const Banner1 = () => {
